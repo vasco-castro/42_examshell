347f6f0f0b9852ff4517afc25a1b62da944144e9
Vasco Castro <vsoares-@c1r10s5.42lisboa.com>
2026-05-08 18:35:09 +0100

    ft_popen fixed

--- rendu/ft_popen/ft_popen.c
+++ rendu/ft_popen/ft_popen.c
@@ -1,43 +1,30 @@
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
 
-int	ft_popen(const char *file, char *const argv[], char type)
+int ft_popen(const char *file, char *const argv[], char type)
 {
 	int fd[2];
 	int pid;
 	int io = 0;
 
-	if (type == 'r')
-		io = 1;
 	if (!file || !argv || (type != 'r' && type != 'w'))
-		return (-1);
+		return -1;
+	if (type == 'w')
+		io = 1;
 	if (pipe(fd) == -1)
-		return (-1);
+		return -1;
 	pid = fork();
 	if (pid == -1)
-		return (close(fd[0]), close(fd[1]), -1);
+		return (close(fd[io]), close(fd[!io]), -1);
 	if (pid == 0)
 	{
-		puts("child");
 		dup2(fd[io], io);
-		close(fd[0]);
-		close(fd[1]);
+		close(fd[io]);
+		close(fd[!io]);
 		execvp(file, argv);
 		exit(1);
 	}
-	puts("psar");
-	return (close(fd[io]), !io);
-}
-
-int main(void)
-{
-	int fd = ft_popen("ls", (char *[]) {"ls", NULL}, 'r');
-	static char buf[4096];
-
-	int size = read(fd, buf, 4096);
-
-	printf("|BUF: %d %s|\n", size, buf);
-
-	return 0;
+	return (close(fd[io]), fd[!io]);
 }
